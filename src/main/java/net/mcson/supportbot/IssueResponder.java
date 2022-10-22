486b1dfffc48dca156394d97b247bb19ef814ab3
Steven Tracey <steven@nevets.tech>
2022-10-22 18:16:52 -0400

    Fix linux

--- src/main/java/net/mcson/supportbot/IssueResponder.java
+++ src/main/java/net/mcson/supportbot/IssueResponder.java
@@ -1,6 +1,8 @@
 package net.mcson.supportbot;
 
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.io.File;
@@ -10,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class IssueResponder {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IssueResponder.class);
     private static List<Keyword> responses = new ArrayList<>();
 
     public static boolean containsKeyword(String message) {
@@ -33,6 +36,7 @@ public class IssueResponder {
 
     public static void addKeyword(Keyword keyword) {
         responses.add(keyword);
+        LOGGER.info("Keyword added to array");
     }
 
     public static void loadResponses() {
@@ -43,6 +47,7 @@ public class IssueResponder {
                 Gson gson = new Gson();
                 for (File file : files) {
                     responses.add(gson.fromJson(new FileReader(file), Keyword.class));
+                    LOGGER.info("Loaded keyword from: " + file.getAbsolutePath());
                 }
             }
         } catch (IOException e) {
